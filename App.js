96111da7bf1dcc7cb513998f0f168521348cb557
hyungnam <zzzsh789@naver.com>
2020-08-12 23:00:17 +0900

    initial commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>오형남의 첫 번째 앱이다!!</Text>
       <StatusBar style="auto" />
     </View>
   );
